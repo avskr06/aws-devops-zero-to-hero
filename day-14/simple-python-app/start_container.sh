00e48706b28abc520ebe086ce4b791951a9b2cbc
sree <144780588+avskr06@users.noreply.github.com>
2023-09-29 23:24:10 +0530

    modified file in floder

--- day-14/simple-python-app/start_container.sh
+++ day-14/simple-python-app/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull anilsree/simple-python-flask-app:latest
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 anilsree/simple-python-flask-app:latest
