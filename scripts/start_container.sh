07c12fcc3f64dbe4281534b0d4b3f0528cee7816
sree <144780588+avskr06@users.noreply.github.com>
2024-01-20 17:23:37 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -3,7 +3,7 @@ set -e
 
 
 # Pull the Docker image from Docker Hub
-docker pull anilsree/simple-python-flask-app:latest
+docker pull anilsree/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 anilsree/simple-python-flask-app:latest
+docker run -d -p 5000:5000 anilsree/simple-python-flask-app
